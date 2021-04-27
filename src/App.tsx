6de4a282fd7a3b9b86e144ac8e28bab84e708aa6
Histefanhere <stefan.zdra@gmail.com>
2021-04-27 14:12:27 +1200

    Button and App styling This is mostly for the validate button, but there's some other styling like the bg colour.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Padlock />
+      <footer>Made with ❤️ by <a href="https://github.com/histefanhere" target="_blank" rel="noopener noreferrer">Histefanhere</a> for a <a href="https://github.com/NZMSA/2021-introduction-to-general-software-development" target="_blank" rel="noopener noreferrer">NZMSA Workshop Competition</a></footer>
     </div>
   );
 }
